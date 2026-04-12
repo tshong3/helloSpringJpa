02c5e51064c81db3ec164e98df1f9b6f1cecb4b5
tshong3 <tshong3@naver.com>
2026-04-12 11:56:15 +0900

    feat: Category 목록 조회 기능 구현

---
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package kr.ac.hansung.cse.controller;
+
+import kr.ac.hansung.cse.service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "categoryList";
+    }
+}
