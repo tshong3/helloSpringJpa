c9891e7cdc091358e660116d067c27899b92d36c
tshong3 <tshong3@naver.com>
2026-04-12 12:08:26 +0900

    feat: Category 등록 폼 화면 구현

--- src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import kr.ac.hansung.cse.model.CategoryForm;
 
 @Controller
 @RequestMapping("/categories")
@@ -20,5 +21,11 @@ public class CategoryController {
     public String listCategories(Model model) {
         model.addAttribute("categories", categoryService.getAllCategories());
         return "categoryList";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("categoryForm", new CategoryForm());
+        return "categoryForm";
     }
 }
